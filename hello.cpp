26df392ea6020d89ddc8e73f235febee0e4275c8
dkdrozdov <DKDrozdov@gmail.com>
2018-04-02 19:47:10 +0600

    initial project version

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include "header.h"
+
+int main(){
+
+	char string[10];
+	scanf("%s", string);
+	printstring(string);
+	return 0;
+}
---
+++ printstring.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+
+int printstring(const char *string){
+	printf("%s", string);
+	return 0;
+}
